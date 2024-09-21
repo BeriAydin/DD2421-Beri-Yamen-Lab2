03d959eabbb91c63bdd04f18072d725ce9d14981
YamenKTH <90331808+YamenKTH@users.noreply.github.com>
2024-09-21 14:58:34 +0200

    second push*

--- main.py
+++ main.py
@@ -6,27 +6,44 @@ import matplotlib.pyplot as plt
 
 # 1. Generating the data set
 
-classA = numpy. concatenate (
-(numpy.random. randn(10 , 2) ∗ 0.2 + [1.5 , 0.5] ,
-numpy.random. randn(10 , 2) ∗ 0.2 + [ −1.5 , 0.5]))
-classB = numpy.random. randn(20 , 2) ∗ 0.2 + [0.0 , −0.5]
+classA = numpy.concatenate (
+    (numpy.random. randn(10 , 2) * 0.2 + [1.5 , 0.5] ,
+     numpy.random. randn(10 , 2) * 0.2 + [ -1.5 , 0.5]))
+classB = numpy.random. randn(20 , 2) * 0.2 + [0.0 , -0.5]
+
 inputs = numpy. concatenate (( classA , classB ))
 targets = numpy. concatenate (
-(numpy. ones ( classA . shape [0]) ,
-−numpy. ones ( classB . shape [0])))
-N = inputs . shape [0] # Number o f rows ( s a m p l e s )
-permute=l i s t (range (N))
-random. shuffle (permute)
-inputs = inputs [ permute , : ]
+    (numpy. ones ( classA . shape [0]) ,
+     -numpy. ones ( classB . shape [0])))
 
-def ind(S)
-    
+N = inputs . shape [0] # Number of rows (samples)
+
+permute=list(range (N))
+random. shuffle(permute)
+inputs = inputs[permute, : ]
+targets = targets[permute ]
+
+
+def ind(S):
+    # It is used to determine the class of the input. + value means 1 class, - value means -1 class.
+    return 0
 
 def linear_kernel(x, y):
+    # This kernel simply returns the scalar product between the two points. This results in a linear separation.
+    return 0
 
-    return numpy.dot(x, y)
+def polynomial_kernel(x, y, p):
+    # This kernel allows for curved decision boundaries. The exponent p (a positive integer) controls the degree of the polynomials. p = 2 will make quadratic shapes (ellipses, parabolas, hyperbolas). Setting p = 3 or higher will result in more complex shapes.
+    return (1 + numpy.dot(x, y)) ** p
 
+def RBF_kernel(x, y, sigma=5.0):
+    # radial basis function kernel. This kernel uses the explicit euclidian distance between the two datapoints, and often results in very good boundaries. The parameter σ is used to control the smoothness of the boundary
+    return 0
 
 def objective (alpha):
     # returns a scalar value, effectively implementing the expression should be minimized in equation (4)
+    return 0
+
+def zerofun(alpha):
+    # This function should implement the equality constraint of (10). Also here, you can make use of numpy.dot to be efficient.
     return 0
