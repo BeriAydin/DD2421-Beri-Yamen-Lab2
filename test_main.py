7f493fef3fc9af2a101e282375ac05b3b5082f62
Beri Yesil Aydin <beri.aydin@gmail.com>
2024-09-30 21:14:19 +0200

    nu funkar den

--- test_main.py
+++ test_main.py
@@ -3,7 +3,7 @@ from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 
 
-
+np.random.seed(100)
 # 5. Generating the data set
 classA = np.concatenate (
     (np.random. randn(10 , 2) * 0.2 + [1.5 , 0.5] ,
@@ -22,37 +22,33 @@ random. shuffle(permute)
 inputs = inputs[permute, : ]
 targets = targets[permute ]
  
-def linear_kernel(x, y):
-    # This kernel simply returns the scalar product between the two points. This results in a linear separation.
-    return np.dot(x, y)
+k_type = 'linear'
 
-def polynomial_kernel(x, y, p):
-    # This kernel allows for curved decision boundaries. The exponent p (a positive integer) controls the degree of the polynomials. p = 2 will make quadratic shapes (ellipses, parabolas, hyperbolas). Setting p = 3 or higher will result in more complex shapes.
-    return (1 + np.dot(x, y)) ** p
+def K(x,y, kernel_type):
+    p = 2
+    sigma = 1
+    if kernel_type == 'linear':
+        return np.dot(x, y)
 
-def RBF_kernel(x, y, sigma=5.0):
-    # radial basis function kernel. This kernel uses the explicit euclidian distance between the two datapoints, and often results in very good boundaries. The parameter σ is used to control the smoothness of the boundary
-    return math.exp(-np.linalg.norm(x-y)**2/(2*sigma))
-K = linear_kernel       ######################################### Change the kernel here ############################################
+    elif kernel_type == 'polynomial':
+        return (1 + np.dot(x, y)) ** p
 
-def calculate_matrix(X, t, K, parameter = None):
+    elif kernel_type == 'RBF':
+        return math.exp(-np.linalg.norm(x-y)**2/(2*sigma))
+
+def calculate_matrix(X, t):
     P = np.zeros((N , N))
-    if parameter is None:
-        for i in range(N):
-            for j in range(N):
-                P[i,j] = t[i]*t[j]*K(X[i], X[j])
-    else:
-        for i in range(N):
-            for j in range(N):
-                P[i,j] = t[i]*t[j]*K(X[i], X[j], parameter)
+    for i in range(N):
+        for j in range(N):
+            P[i,j] = t[i]*t[j]*K(X[i], X[j], k_type)
     return P
 
-P = calculate_matrix(inputs, targets, K)
+P = calculate_matrix(inputs, targets)
 
 
-def objective (alpha):
+def objective(alpha):
     # returns a scalar value, effectively implementing the expression should be minimized in equation (4)
-    return 0.5* np.dot(alpha, np.dot(P, alpha)) - np.sum(alpha)
+    return (1/2)*np.dot(alpha, np.dot(alpha, P)) - np.sum(alpha)
 
 def zerofun(alpha):
     # This function should implement the equality constraint of (10). Also here, you can make use of np.dot to be efficient.
@@ -66,12 +62,7 @@ ret = minimize( objective , start , bounds=[(0, C) for b in range(N)], constrain
 alpha = ret ['x']
 
 # extract the non-zero α values that are greater than 10^-5 and save them as a np array along with their corresponding input and target values.
-alpha_nonzeros = []
-alpha_threshold = 10**-5
-for i in range(N):
-    if alpha[i] > alpha_threshold:
-        alpha_nonzeros.append((alpha[i], inputs[i], targets[i]))
-
+alpha_nonzeros = [(alpha[i], inputs[i], targets[i]) for i in range(N) if abs(alpha[i]) > 10e-5]
 
 
 print("Alpha values:", alpha)
@@ -79,29 +70,22 @@ print("Optimization success:", ret['success'])
 print("Message:", ret['message'])
 
 # calculate the bias term b using equation (7) for each of the support vectors.
-def calculate_b(K, parameter = None):
-    s = alpha_nonzeros[0] # choose the first support vector
-    temp_b = 0
-    for i in range(len(alpha_nonzeros)):
-        temp_b += alpha_nonzeros[i][0]*alpha_nonzeros[i][2]*K(alpha_nonzeros[i][1], s[1])
-    temp_b -= s[2]
-    return temp_b
+def calculate_b():
+    bsum = 0
+    for value in alpha_nonzeros:
+        bsum += value[0] * value[2] * K(value[1], alpha_nonzeros[0][1], k_type)
+    return bsum - alpha_nonzeros[0][2]
 
-b = calculate_b(K)
+b = calculate_b()
+print(b)
 
 
-def ind(S, K, parameter = None):
+def ind(S, K):
     # It is used to determine the class of the input. + value means 1 class, - value means -1 class.
-    temp_class = 0
-    if parameter is None:
-        for sample in alpha_nonzeros:
-            temp_class += sample[0]*sample[2]*K(S, sample[1])
-        temp_class -= b
-    else:
-        for sample in alpha_nonzeros:
-            temp_class += sample[0]*sample[2]*K(S, sample[1], sample)
-        temp_class -= b
-    return temp_class
+    totsum = 0
+    for value in alpha_nonzeros:
+        totsum += value[0] * value[2] * K(S, value[1],k_type)
+    return totsum - b
 
 # 6. Plotting the data set
 plt.plot([p[0] for p in classA],
@@ -110,7 +94,7 @@ plt.plot([p[0] for p in classA],
 plt.plot([p[0] for p in classB],
         [p[1] for p in classB],
         'r.')
-plt.axis('equal') # Force same scale on both axes
+#plt.axis('equal') # Force same scale on both axes
 #plt.savefig('svmplot.pdf') # Save a copy in a file 
 #plt.show() # Show the plot on the screen
 
@@ -129,6 +113,10 @@ plt.plot([p[0] for p in classA],
 plt.plot([p[0] for p in classB],
             [p[1] for p in classB],
             'r.')
-#plt.axis('equal') # Force same scale on both axes
+plt.axis('equal') # Force same scale on both axes
 plt.savefig('svmplot.pdf') # Save a copy in a file
 plt.show() # Show the plot on the screen
+
+
+print(ind(classA[0],K), 'A')
+print(ind(classB[0],K), 'B')
